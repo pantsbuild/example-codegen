dca26647768895edefd1b45ecca64a19c2d76fdd
Tom Dyas <tdyas@toolchain.com>
2022-01-25 20:49:42 +0000

    add comments

--- src/python/thrift_examples/person_test.py
+++ src/python/thrift_examples/person_test.py
@@ -1,3 +1,11 @@
+# Note: Apache Thrift generates mutiple Python packages from a single Thrift file. The base name
+# of those packages comes from the value of the `namespace` directive in the Thrift file.
+# `constants` and `ttypes` subpackages are generated under that base name.
+#
+# In this case, the `namespace` directive is `thrift_example.gen`. This, Apache Thrift generates two
+# packages: `thrift_example.gen.constants` and `thrift_example.gen.ttypes`. This file then imports
+# the `Person` class from `thrift_example.gen.ttypes`.
+
 from thrift_example.gen.ttypes import Person
 
 def test_person() -> None:
--- src/thrift/person.thrift
+++ src/thrift/person.thrift
@@ -1,3 +1,6 @@
+# Note: Apache Thrift generates mutiple Python packages from a single Thrift file.
+# `constants` and `ttypes` subpackages are generated under that base name. The base name
+# of those packages comes from the value of the following `namespace` directive:
 namespace py thrift_example.gen
 
 struct Person {
