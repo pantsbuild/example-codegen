028aa57caf03d6d1a762b7ba9f50fef354ae9b59
Tom Dyas <tdyas@toolchain.com>
2022-03-14 12:41:28 -0400

    checkpoint

---
+++ src/jvm/org/pantsbuild/example/codegen/scala/PersonSample.scala
@@ -0,0 +1,15 @@
+package org.pantsbuild.example.codegen.scala
+
+import org.pantsbuild.example.codegen.protos.Person
+
+object PersonSample {
+  def main(args: Array[String]): Unit = {
+    val p = Person(
+      name = "Someone",
+      id = 1234,
+      email = "someone@example.com",
+    )
+    println(p)
+  }
+}
+
